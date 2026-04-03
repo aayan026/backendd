13ba9298a1923ecd7073e87b5cb2bcc839cee6b6
Ayan Aliyeva <aliyevayan1929@gmail.com>
2026-04-03 08:18:09 +0400

    db changed

--- Core/FurnitureShop.Domain/Entities/Concretes/CollectionCategory.cs
+++ Core/FurnitureShop.Domain/Entities/Concretes/CollectionCategory.cs
@@ -7,6 +7,7 @@ public class CollectionCategory : BaseEntity
 {
     public string ImageUrl { get; set; } = null!;
     public decimal TotalPrice { get; set; }
+    public int collectionCount { get; set; }
     public ICollection<CollectionCategoryTranslation> Translations { get; set; } = new List<CollectionCategoryTranslation>();
     public ICollection<Collection> Collections { get; set; } = new List<Collection>();
 }
--- Infrastructure/FurnitureShop.Persistence/Migrations/20260403041605_init322.Designer.cs
+++ Infrastructure/FurnitureShop.Persistence/Migrations/20260403041605_init322.Designer.cs
@@ -4,6 +4,7 @@ using FurnitureShop.Persistence.Datas;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FurnitureShop.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260403041605_init322")]
+    partial class init322
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
